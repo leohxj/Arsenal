8657a736652567c395b83423eb99869f87cc8efe
Leo Hui <leohxj@gmail.com>
2016-08-20 21:58:25 +0800

    modify setup.sh

--- dotfiles/Mac/setup.sh
+++ dotfiles/Mac/setup.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
+############################
+# .setup.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
 
-# This symlinks all the dotfiles (and .atom/) to ~/
-# It also symlinks ~/bin for easy updating
+########## Variables
 
-# This is safe to run multiple times and will prompt you about anything unclear
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="npmrc zshrc gitconfig"    # list of files/folders to symlink in homedir
 
+##########
 
-#
-# Utils
-#
-echo "hello setup"
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
