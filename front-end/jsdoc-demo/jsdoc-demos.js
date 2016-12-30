e6227cb75f6afcd765e46b12162dae3e7a953fda
Leo Hui <leohxj@gmail.com>
2016-12-30 14:21:02 +0800

    update useage of @memberof for IFFE function

--- front-end/jsdoc-demo/jsdoc-demos.js
+++ front-end/jsdoc-demo/jsdoc-demos.js
@@ -1,69 +1,26 @@
-/**
- * Add two numbers.
- * @param {number} num1 The first number.
- * @param {number} num2 The second number.
- * @returns {number} The sum of the two numbers.
- */
-function add(num1, num2) {
-    return num1 + num2;
-}
 
-// default options allow missing function description
-// return type `void` means the function has no `return` statement
 /**
- * @param {string} name Whom to greet.
- * @returns {void}
+ * My namespace.
+ * @namespace MyNamespace
  */
-function greet(name) {
-    console.log("Hello " + name);
-}
-
-// @constructor tag allows missing @returns tag
-/**
- * Represents a sum.
- * @constructor
- * @param {number} num1 The first number.
- * @param {number} num2 The second number.
- */
-function sum(num1, num2) {
-    this.num1 = num1;
-    this.num2 = num2;
-}
-
-// class constructor allows missing @returns tag
-/**
- * Represents a sum.
- */
-class Sum {
+(function() {
     /**
-     * @param {number} num1 The first number.
-     * @param {number} num2 The second number.
+     * fff
+     *
+     * @memberof MyNamespace
+     * @param {string} s The skfsl
      */
-    constructor(num1, num2) {
-        this.num1 = num1;
-        this.num2 = num2;
+    function fff(s) {
+        return "xx"
     }
+})();
+
+
+/**
+ * @param {string} somebody
+ */
+function sayHello(somebody) {
+    alert('Hello ' + somebody);
 }
 
-// @abstract tag allows @returns tag without `return` statement
-class Widget {
-    /**
-    * When the state changes, does it affect the rendered appearance?
-    * @abstract
-    * @param {Object} state The new state of the widget.
-    * @returns {boolean} Is current appearance inconsistent with new state?
-    */
-    mustRender (state) {
-        throw new Error("Widget subclass did not implement mustRender");
-    }
-}
-
-// @override tag allows missing @param and @returns tags
-class WonderfulWidget extends Widget {
-    /**
-     * @override
-     */
-    mustRender (state) {
-        return state !== this.state; // shallow comparison
-    }
-}
+var x = 10;
